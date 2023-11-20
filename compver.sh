0ceb672e7aa99d3c6bce1140a35f166ad664c031
Corlatan Philip (XC-DX/EHS3) <cop5fe@SI-V-0000J.brt.emea.bosch.com>
2023-11-20 12:34:34 +0100

    Added version compare script

---
+++ compver.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Fetch all tags from the remote repository
+git fetch --tags
+
+# Get the newest version tag in the format number.number.number that exists in the current branch
+current_version=$(git log HEAD --oneline --decorate | grep -Eo '\b[0-9]+\.[0-9]+\.[0-9]+\b' | sort -V | tail -n 1)
+
+# Get the newest version tag in the develop branch
+develop_version=$(git log origin/develop --oneline --decorate | grep -Eo '\b[0-9]+\.[0-9]+\.[0-9]+\b' | sort -V | tail -n 1)
+
+# Compare versions
+if [[ $(printf "$current_version\n$develop_version" | sort -V | tail -n 1) == "$current_version" ]]; then
+    echo "Die aktuelle Version ($current_version) ist größer als die Version im develop-Zweig ($develop_version)."
+else
+    echo "Die aktuelle Version ($current_version) ist nicht größer als die Version im develop-Zweig ($develop_version)."
+fi
+
