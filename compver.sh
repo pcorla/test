0b48e6535d53ce667f85d96cb21e351cf011a1bd
Corlatan Philip (XC-DX/EHS3) <cop5fe@SI-V-0000J.brt.emea.bosch.com>
2023-11-20 13:19:04 +0100

    Moved version check to script

--- compver.sh
+++ compver.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-# Fetch all tags from the remote repository
+
 git fetch --tags
-
-# Get the newest version tag in the format number.number.number that exists in the current branch
 current_version=$(git log HEAD --oneline --decorate | grep -Eo '\b[0-9]+\.[0-9]+\.[0-9]+\b' | sort -V | tail -n 1)
-
-# Get the newest version tag in the develop branch
 develop_version=$(git log origin/develop --oneline --decorate | grep -Eo '\b[0-9]+\.[0-9]+\.[0-9]+\b' | sort -V | tail -n 1)
-
-# Compare versions
 if dpkg --compare-versions "$current_version" gt "$develop_version"; then
-    echo "Die aktuelle Version ($current_version) ist größer als die Version im develop-Zweig ($develop_version)."
+  echo "Version ($current_version) is greater than develop version ($develop_version)."
 else
-    echo "Die aktuelle Version ($current_version) ist nicht größer als die Version im develop-Zweig ($develop_version)."
+  echo "Version ($current_version) is not greater than develop version ($develop_version)."
+  exit 1
 fi
 
