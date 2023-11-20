4112909e47dcf7a82678b26d6251a90e4642a37f
Corlatan Philip (XC-DX/EHS3) <cop5fe@SI-V-0000J.brt.emea.bosch.com>
2023-11-20 13:58:37 +0100

    Test workflow vars

--- compver.sh
+++ compver.sh
@@ -4,6 +4,8 @@
 git fetch --tags
 current_version=$(git log HEAD --oneline --decorate | grep -Eo '\b[0-9]+\.[0-9]+\.[0-9]+\b' | sort -V | tail -n 1)
 develop_version=$(git log origin/develop --oneline --decorate | grep -Eo '\b[0-9]+\.[0-9]+\.[0-9]+\b' | sort -V | tail -n 1)
+current_version=A
+develop_version=B
 if dpkg --compare-versions "$current_version" gt "$develop_version"; then
   echo "Version ($current_version) is greater than develop version ($develop_version)."
 else
