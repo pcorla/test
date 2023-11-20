6a66be82cee038519445cd68c809e4c34f009170
Corlatan Philip (XC-DX/EHS3) <cop5fe@SI-V-0000J.brt.emea.bosch.com>
2023-11-20 14:49:20 +0100

    Removed debug output

--- compver.sh
+++ compver.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 
-
 git fetch --tags
-echo "git log HEAD"
-git log HEAD --oneline --decorate
-
-echo "git log HEAD version"
-git log HEAD --oneline --decorate | grep -Eo '\b[0-9]+\.[0-9]+\.[0-9]+\b' | sort -V | tail -n 1
-
-echo "git log develop"
-git log origin/develop --oneline --decorate | grep -Eo '\b[0-9]+\.[0-9]+\.[0-9]+\b' | sort -V | tail -n 1
-
-echo "git branch"
-git branch
-
 echo "git log write to vars"
 current_version=$(git log HEAD --oneline --decorate | grep -Eo '\b[0-9]+\.[0-9]+\.[0-9]+\b' | sort -V | tail -n 1)
 develop_version=$(git log origin/develop --oneline --decorate | grep -Eo '\b[0-9]+\.[0-9]+\.[0-9]+\b' | sort -V | tail -n 1)
